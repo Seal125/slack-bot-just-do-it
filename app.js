781d6339d9837c9d43631e5d1347bcebd30d26ab
Stephanie Smith <stephaniesmith12514@gmail.com>
2022-02-10 09:11:08 -0500

    Sets up basic bot server and bot response

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+
+const { App } = require('@slack/bolt');
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000
+});
+
+app.message('hello', async ({ message, say }) => {
+    await say({
+        blocks: [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": `Hey there <@${message.user}>!`
+            },
+            "accessory": {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "text": "Click Me"
+              },
+              "action_id": "button_click"
+            }
+          }
+        ],
+        text: `Hey there <@${message.user}>!`
+      });
+  });
+
+(async () => {
+  // Start your app
+  await app.start();
+
+  console.log('⚡️ Bolt app is running!');
+})();
