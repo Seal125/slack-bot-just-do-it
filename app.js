4de7ae305419fcec840e90d11c010b377acd6aa3
Stephanie Smith <stephaniesmith12514@gmail.com>
2022-02-12 11:49:11 -0500

    Adds bot shortcut to add todo, formats modal

--- app.js
+++ app.js
@@ -11,27 +11,128 @@ const app = new App({
   port: process.env.PORT || 3000
 });
 
-app.message('hello', async ({ message, say }) => {
-    await say({
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": `Hey there <@${message.user}>!`
-            },
-            "accessory": {
-              "type": "button",
-              "text": {
-                "type": "plain_text",
-                "text": "Click Me"
-              },
-              "action_id": "button_click"
+app.event('message', async ({ event, client, logger }) => {
+    console.log('event', event)
+    try {
+      if (event.text === RegExp(/add/i)) {
+
+      }
+    }
+    catch (error) {
+      logger.error(error);
+    }
+  });
+
+app.shortcut('add_todo', async ({ ack, payload, client }) => {
+    await ack()
+
+    try {
+        const todo = await client.views.open({
+            trigger_id: payload.trigger_id,
+            view: {
+                "type": "modal",
+                "title": {
+                    "type": "plain_text",
+                    "text": "My App",
+                    "emoji": true
+                },
+                "submit": {
+                    "type": "plain_text",
+                    "text": "Submit",
+                    "emoji": true
+                },
+                "close": {
+                    "type": "plain_text",
+                    "text": "Cancel",
+                    "emoji": true
+                },
+                "blocks": [
+                    {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Add a Todo",
+                            "emoji": true
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "input",
+                        "element": {
+                            "type": "plain_text_input",
+                            "action_id": "plain_text_input-action"
+                        },
+                        "label": {
+                            "type": "plain_text",
+                            "text": "Title",
+                            "emoji": true
+                        }
+                    },
+                    {
+                        "type": "input",
+                        "element": {
+                            "type": "plain_text_input",
+                            "action_id": "plain_text_input-action"
+                        },
+                        "label": {
+                            "type": "plain_text",
+                            "text": "Description",
+                            "emoji": true
+                        }
+                    },
+                    {
+                        "type": "input",
+                        "element": {
+                            "type": "datepicker",
+                            "initial_date": "1990-04-28",
+                            "placeholder": {
+                                "type": "plain_text",
+                                "text": "Select a date",
+                                "emoji": true
+                            },
+                            "action_id": "datepicker-action"
+                        },
+                        "label": {
+                            "type": "plain_text",
+                            "text": "Due Date",
+                            "emoji": false
+                        }
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "checkboxes",
+                                "options": [
+                                    {
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Already Completed!",
+                                            "emoji": false
+                                        },
+                                        "value": "completed"
+                                    }
+                                ],
+                                "action_id": "complete"
+                            }
+                        ]
+                    }
+                ]
             }
-          }
-        ],
-        text: `Hey there <@${message.user}>!`
-      });
+        })
+        console.log("resulting todo: ", todo)
+    }
+    catch(err) {
+        console.error(err)
+    }
+  });
+
+  app.action('button_click', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    await say(`<@${body.user.id}> clicked the button`);
   });
 
 (async () => {
